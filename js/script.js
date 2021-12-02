38a5315fb9e2c0212ac826a72e7cc7e09c3ed126
Aleksey <a.b.puzanov@gmail.com>
2021-12-02 21:23:18 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Деректива, которая указывает что мы работаем в современном режиме с новыми стандартами
+"use strict";
+//новый стандарт
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+
+
+
+const user = "Ivan";
+
+alert (`Привет,${user}`);
