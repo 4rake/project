5502d6f0bd80d0bc2cc2ea1f76ccc4caf1e6ee16
Aleksey <a.b.puzanov@gmail.com>
2021-12-22 12:53:23 +0300

    ВВОД

--- js/script.js
+++ js/script.js
@@ -1,33 +1,8 @@
-// Деректива, которая указывает что мы работаем в современном режиме с новыми стандартами
-"use strict";
+'use strict';
 
+// To string (1)
 
-console.log('arr' + "- object");
+console.log(typeof(String(null)));
+console.log(typeof(String(4)));
 
-
-console.log(4 + "5");
-
-let incr = 10,
-    decr = 10;
-
-
-
-console.log(++incr);
-console.log(--decr);
-
-
-console.log(5%2);
-
-const isCheket = false,
-      isClose = true;
-
-console.log(isCheket || !isClose);
-
-
-
-console.log(2 + 2 * 2 !== '6');
-
-
-
-
-
+//2
